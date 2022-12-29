63833463fdb64b2201af39e43c632be5c9bbabf8
Minxrod <minxrod@gmail.com>
2022-12-29 14:44:53 -0500

    Convert spaces to tabs

--- cetrisdt_edit.py
+++ cetrisdt_edit.py
@@ -7,152 +7,152 @@ tilemap = []
 
 
 def edit_help(args):
-    if len(args) > 1:
-        help_cmd = args[1]
+	if len(args) > 1:
+		help_cmd = args[1]
 
-        for command in commands:
-            if help_cmd == command[0]:
-                print(command[0] + " " + command[1])
-    else:
-        print("List of commands: ")
-        for command in commands:
-            print(command[0] + " " + command[1])
+		for command in commands:
+			if help_cmd == command[0]:
+				print(command[0] + " " + command[1])
+	else:
+		print("List of commands: ")
+		for command in commands:
+			print(command[0] + " " + command[1])
 
 
 def palette(args):
-    global pal
+	global pal
 
-    b = 8
-    if len(args) > 2:
-        b = int(args[2])
+	b = 8
+	if len(args) > 2:
+		b = int(args[2])
 
-    pal = converter.convert_file_to_palette(args[1], b=b)  # generate.convert_file_to_palette(args[1])
+	pal = converter.convert_file_to_palette(args[1], b=b)  # generate.convert_file_to_palette(args[1])
 
 
 def tile(args):
-    global data
+	global data
 
-    # if pal is None:
-    #     palette(args)
+	# if pal is None:
+	#	 palette(args)
 
-    data = [converter.convert_file_simple(args[1], pal)]  # generate.convert_file_to_data(args[1], pal)
+	data = [converter.convert_file_simple(args[1], pal)]  # generate.convert_file_to_data(args[1], pal)
 
 
 def tileset(args):
-    global tiles, data
+	global tiles, data
 
-    tiles = []
-    data = []
+	tiles = []
+	data = []
 
-    img_dat = converter.load_file(args[1])
-    pix = img_dat[0]
-    w = img_dat[1]
-    h = img_dat[2]
+	img_dat = converter.load_file(args[1])
+	pix = img_dat[0]
+	w = img_dat[1]
+	h = img_dat[2]
 
-    if len(args) > 2:
-        size = int(args[2])
-    else:
-        size = 16
+	if len(args) > 2:
+		size = int(args[2])
+	else:
+		size = 16
 
-    b = len(pal[0])
-    for y in range(0, h, size):
-        for x in range(0, w, size):
-            tile_data = converter.convert_image_to_data(pix, x, y, size, size, pal, adjust=False)
-            tile_adjusted = converter.adjust_data(tile_data, b)
-            tile_palette = min(tile_data) // b * b
+	b = len(pal[0])
+	for y in range(0, h, size):
+		for x in range(0, w, size):
+			tile_data = converter.convert_image_to_data(pix, x, y, size, size, pal, adjust=False)
+			tile_adjusted = converter.adjust_data(tile_data, b)
+			tile_palette = min(tile_data) // b * b
 
-            if tile_adjusted not in data:  # help prevent palette swaps from hogging memory
-                data.append(tile_adjusted)
-            tiles.append((data.index(tile_adjusted), tile_palette))
+			if tile_adjusted not in data:  # help prevent palette swaps from hogging memory
+				data.append(tile_adjusted)
+			tiles.append((data.index(tile_adjusted), tile_palette))
 
 
 def load_map(args):
-    global tilemap
+	global tilemap
 
-    tilemap = []
+	tilemap = []
 
-    img_dat = converter.load_file(args[1])
-    pix = img_dat[0]
-    w = img_dat[1]
-    h = img_dat[2]
+	img_dat = converter.load_file(args[1])
+	pix = img_dat[0]
+	w = img_dat[1]
+	h = img_dat[2]
 
-    if len(args) > 2:
-        size = int(args[2])
-    else:
-        size = 16
+	if len(args) > 2:
+		size = int(args[2])
+	else:
+		size = 16
 
-    b = len(pal[0])
-    for y in range(0, h, size):
-        tilemap.append([])
-        for x in range(0, w, size):
-            tile_data = converter.convert_image_to_data(pix, x, y, size, size, pal, adjust=False)
-            tile_adjusted = converter.adjust_data(tile_data, b)
-            tile_palette = min(tile_data) // b * b
+	b = len(pal[0])
+	for y in range(0, h, size):
+		tilemap.append([])
+		for x in range(0, w, size):
+			tile_data = converter.convert_image_to_data(pix, x, y, size, size, pal, adjust=False)
+			tile_adjusted = converter.adjust_data(tile_data, b)
+			tile_palette = min(tile_data) // b * b
 
-            tile_info = (data.index(tile_adjusted), tile_palette)
-            tilemap[-1].append(tiles.index(tile_info))
+			tile_info = (data.index(tile_adjusted), tile_palette)
+			tilemap[-1].append(tiles.index(tile_info))
 
 
 def output(args):
-    offset = offset2 = 0
-    if len(args) > 2:
-        offset = int(args[2])
-    if len(args) > 3:
-        offset2 = int(args[3])
+	offset = offset2 = 0
+	if len(args) > 2:
+		offset = int(args[2])
+	if len(args) > 3:
+		offset2 = int(args[3])
 
-    convdat = None
-    if args[1] == "palette":
-        if pal is not None:
-            conv_pal = []
-            for subpal in pal:
-                conv_pal.append([])
-                for item in subpal:
-                    val = (int(item[0] / 8) << 10) + (int(item[1] / 8) << 5) + (int(item[2] / 8))
-                    conv_pal[-1].append(val % 256)
-                    conv_pal[-1].append(val // 256)
+	convdat = None
+	if args[1] == "palette":
+		if pal is not None:
+			conv_pal = []
+			for subpal in pal:
+				conv_pal.append([])
+				for item in subpal:
+					val = (int(item[0] / 8) << 10) + (int(item[1] / 8) << 5) + (int(item[2] / 8))
+					conv_pal[-1].append(val % 256)
+					conv_pal[-1].append(val // 256)
 
-            convdat = converter.convert_lists_to_db(conv_pal, 8)
-    elif args[1] == "tile":
-        if data is not None:
-            convdat = converter.convert_lists_to_db(data, converter.col[len(pal[0])])
-    elif args[1] == "tileset":
-        if tiles is not None:
-            convdat = converter.convert_lists_to_db([[x[0] + offset, x[1] + offset2] for x in tiles], 8)
-    elif args[1] == "map":
-        if tilemap is not None:
-            convdat = converter.convert_lists_to_db([[y + offset for y in x] for x in tilemap], 8)
-    else:
-        print("Error: Invalid data type\nPlease choose from: palette, tile, tileset, map")
-        return
+			convdat = converter.convert_lists_to_db(conv_pal, 8)
+	elif args[1] == "tile":
+		if data is not None:
+			convdat = converter.convert_lists_to_db(data, converter.col[len(pal[0])], per_line=offset if offset else None)
+	elif args[1] == "tileset":
+		if tiles is not None:
+			convdat = converter.convert_lists_to_db([[x[0] + offset, x[1] + offset2] for x in tiles], 8)
+	elif args[1] == "map":
+		if tilemap is not None:
+			convdat = converter.convert_lists_to_db([[y + offset for y in x] for x in tilemap], 8)
+	else:
+		print("Error: Invalid data type\nPlease choose from: palette, tile, tileset, map")
+		return
 
-    print(args[1] + ":\n" + convdat)
+	print(args[1] + ":\n" + convdat)
 
 
 commands = [["help", "[command]", 0, 1, edit_help],
-            ["exit", "", 0, 0, exit],
-            ["palette", "filename [bpp]", 1, 2, palette],
-            ["tile", "filename", 1, 1, tile],
-            ["map", "filename [tile_size]", 1, 2, load_map],
-            ["tileset", "filename [tile_size]", 1, 2, tileset],
-            ["print", "datatype [offset] [offset2]", 1, 3, output]]
+			["exit", "", 0, 0, exit],
+			["palette", "filename [bpp]", 1, 2, palette],
+			["tile", "filename", 1, 1, tile],
+			["map", "filename [tile_size]", 1, 2, load_map],
+			["tileset", "filename [tile_size]", 1, 2, tileset],
+			["print", "datatype [offset] [offset2]", 1, 3, output]]
 
 select = ""
 arg = []
 
 while select != "exit":
-#    try:
-        select = input("? ")
-        
-        arg = select.split()
-        arg_len = len(arg) - 1
-        cmd_len = len(select)
-        
-        for cmd in commands:
-            if cmd[0] == arg[0]:
-                if cmd[2] <= arg_len <= cmd[3]:
-                    cmd[4](arg)
-                else:
-                    print("Wrong number of arguments")
-#    except Exception as e:
-#        print(e)
+	try:
+		select = input("? ")
+		
+		arg = select.split()
+		arg_len = len(arg) - 1
+		cmd_len = len(select)
+		
+		for cmd in commands:
+			if cmd[0] == arg[0]:
+				if cmd[2] <= arg_len <= cmd[3]:
+					cmd[4](arg)
+				else:
+					print("Wrong number of arguments")
+	except Exception as e:
+		print(e)
 
--- converter.py
+++ converter.py
@@ -4,133 +4,134 @@ bpp = {1: 2, 2: 4, 4: 16, 8: 256}
 col = {2: 1, 4: 2, 16: 4, 256: 8}
 
 def load_file(file):
-    img = Image.open(file)
-    pix = img.load()
+	img = Image.open(file)
+	pix = img.load()
 
-    return pix, img.size[0], img.size[1]
+	return pix, img.size[0], img.size[1]
 
 def adjust_data(data: list, b=0):
-    amount = min(data)
-    if b:
-        amount = amount // b * b
-    
-    copy = []
-    for item in data:
-        copy.append(item - amount)
+	amount = min(data)
+	if b:
+		amount = amount // b * b
+	
+	copy = []
+	for item in data:
+		copy.append(item - amount)
 
-    return copy
+	return copy
 
 def convert_file_to_palette(file, b=8):
-    img = Image.open(file)
-    pix = img.load()
+	img = Image.open(file)
+	pix = img.load()
 
-    return convert_image_to_palette(pix, 0, 0, img.size[0], img.size[1], b=b)
+	return convert_image_to_palette(pix, 0, 0, img.size[0], img.size[1], b=b)
 
 def convert_image_to_palette(pix, u, v, w, h, b=8):
-    palette = [[]]
+	palette = [[]]
 
-    counter = 0
+	counter = 0
 
-    for y in range(v, v + h):
-        for x in range(u, u + w):
-            if not pix[x, y] in palette[-1]:
-                palette[-1].append(pix[x, y])
-                counter += 1
-                if counter >= bpp[b]:
-                    counter = 0
-                    palette.append([])
-    try:
-        palette.remove([])
-    except ValueError:
-        pass  #just don't remove it. easy.
+	for y in range(v, v + h):
+		for x in range(u, u + w):
+			if not pix[x, y] in palette[-1]:
+				palette[-1].append(pix[x, y])
+				counter += 1
+				if counter >= bpp[b]:
+					counter = 0
+					palette.append([])
+	try:
+		palette.remove([])
+	except ValueError:
+		pass  #just don't remove it. easy.
 
-    return palette
+	return palette
 
 
 def convert_file_to_data(file, u, v, w, h, palette: list, adjust=True):
-    img = Image.open(file)
-    pix = img.load()
+	img = Image.open(file)
+	pix = img.load()
 
-    return convert_image_to_data(pix, u, v, w, h, palette, adjust=adjust)
+	return convert_image_to_data(pix, u, v, w, h, palette, adjust=adjust)
 
 
 def convert_file_simple(file, palette, adjust=True):
-    img = Image.open(file)
-    pix = img.load()
+	img = Image.open(file)
+	pix = img.load()
 
-    return convert_image_to_data(pix, 0, 0, img.size[0], img.size[1], palette, adjust=adjust)
+	return convert_image_to_data(pix, 0, 0, img.size[0], img.size[1], palette, adjust=adjust)
 
 
 def convert_image_to_data(pix, u, v, w, h, palette: list, adjust=True):
-    data = []
+	data = []
 
-    # find specific palette index for conversion
-    image_pal = convert_image_to_palette(pix, u, v, w, h)[0]  #only one
-    pal_offset = 0
-    pal_id = 0
-    for pal in palette:
-        if set(image_pal) <= set(pal):
-            pal_id = palette.index(pal)
-            pal_offset = pal_id * len(pal)
+	# find specific palette index for conversion
+	image_pal = convert_image_to_palette(pix, u, v, w, h)[0]  #only one
+	pal_offset = 0
+	pal_id = 0
+	for pal in palette:
+		if set(image_pal) <= set(pal):
+			pal_id = palette.index(pal)
+			pal_offset = pal_id * len(pal)
 
-    for y in range(v, v + h):
-        for x in range(u, u + w):
-            if adjust:
-                data.append(palette[pal_id].index(pix[x, y]))
-            else:
-                data.append(pal_offset + palette[pal_id].index(pix[x, y]))
+	for y in range(v, v + h):
+		for x in range(u, u + w):
+			if adjust:
+				data.append(palette[pal_id].index(pix[x, y]))
+			else:
+				data.append(pal_offset + palette[pal_id].index(pix[x, y]))
 
-    return data
+	return data
 
 
-def convert_data_to_db(data, compress):
-    """
-    Converts data in list, uncompressed form,
-    to db string data to be used in compiler.
+def convert_data_to_db(data, compress, per_line=None):
+	"""
+	Converts data in list, uncompressed form,
+	to db string data to be used in compiler.
 
-    :param data: list of data to convert
-    :param compress: how many bits each value is expected to have
-    :return:
-    """
+	:param data: list of data to convert
+	:param compress: how many bits each value is expected to have
+	:return:
+	"""
 
-    data_string = ".db "
+	data_string = ".db "
 
-    db_width = 64 / compress
-    compress_count = 0
-    pixels = 0
+	db_width = 64 / compress
+	per_line = per_line or db_width
+	compress_count = 0
+	pixels = 0
 
-    for db_count, pixel in enumerate(data):
+	for db_count, pixel in enumerate(data):
 
-        compress_count += compress
-        pixels |= pixel << (8 - compress_count)
-        compress_count %= 8
-        if compress_count == 0:
-            data_string += "$" + format(pixels, '02x')
-            pixels = 0
+		compress_count += compress
+		pixels |= pixel << (8 - compress_count)
+		compress_count %= 8
+		if compress_count == 0:
+			data_string += "$" + format(pixels, '02x')
+			pixels = 0
 
-            if db_count < len(data) - 1:
-                if not (db_count + 1) % db_width:
-                    data_string += "\n.db "
-                else:
-                    data_string += ", "
+			if db_count < len(data) - 1:
+				if not (db_count + 1) % per_line:
+					data_string += "\n.db "
+				else:
+					data_string += ", "
 
-    data_string += "\n"
+	data_string += "\n"
 
-    return data_string
+	return data_string
 
-def convert_lists_to_db(data, bpp):
-    """
-    Converts a list of lists into data.
+def convert_lists_to_db(data, bpp, per_line=None):
+	"""
+	Converts a list of lists into data.
 
-    :param data: list of lists to convert
-    :return: data converted to db string
-    """
+	:param data: list of lists to convert
+	:return: data converted to db string
+	"""
 
-    converted_data = ""
-    for sub_list in data:
-        converted_data += (convert_data_to_db(sub_list, bpp))
+	converted_data = ""
+	for sub_list in data:
+		converted_data += (convert_data_to_db(sub_list, bpp, per_line=per_line))
 
-    return converted_data
+	return converted_data
 
 # conv_pal = convert_file_to_palette("images/tpal.png", 2)
 # print(conv_pal)
